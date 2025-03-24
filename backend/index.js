399305ebd88e764f52c13c14f210a268db761831
AnshulVishwa <anshulvishwa2205@gmail.com>
2025-03-24 19:52:02 +0530

    debugginh

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,9 @@ const { ad_info_Router } = require("./Routers/ad_info")
 const app = express()
 const cors = require("cors")
 const { User_Router } = require("./Routers/users")
-<<<<<<< HEAD
+
 const { msgRouter } = require("./Routers/messages")
-=======
->>>>>>> f9625bdc31772df294d4ba701a218199da9c5f94
+
 const PORT = 5000
 Connect_MongoDB()
 
@@ -19,9 +18,8 @@ app.use( express.urlencoded( { extended : false } ) )
 app.use( "/user" , Login_Signup_Router )
 app.use( "/ad_info" , ad_info_Router )
 app.use( "/user" , User_Router )
-<<<<<<< HEAD
+
 app.use( "/msg" , msgRouter )
-=======
->>>>>>> f9625bdc31772df294d4ba701a218199da9c5f94
+
 
 app.listen( PORT , () => console.log(`Server Started at port : ${PORT}`) )

6c8040631402d1a29ff778fe232a29c27f69e23f
VidhiAgrawa <vidhiagrawal2205@gmail.com>
2025-03-24 07:39:37 +0530

    working on homepage

--- frontend/src/Components/HomePage/HomePage.jsx
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import './HomePage.css'
+import Navbar from '../Navbar.jsx'
 function HomePage() {
   return (
     <>
         <main className='homeMain flex'>
           <div className='leftList'>
-            <div className="navbar flex">
-              <div className="profile"></div>
-              <div className="search-bar-home flex">
-                <i class="fa-solid fa-search"></i>
-                <input type="text" className="searchInput" placeholder='Search' />
-              </div>
-              <div className="settings"><i class="fa-solid fa-bars"></i></div>
+            <Navbar/>
+            <div className='SomeBtn'>
+              <button className="all"></button>
+              <button className="Unread"></button>
+              <button className="archieve"></button>
+              <button className="Blocked"></button>
+            </div>
+            
+          </div>
+
+          <div className='rightChat'>
+            <div className="navbarpart2">
             </div>
           </div>
-          <div className='rightChat'>hello</div>
         </main>
     </>
   )
---
+++ frontend/src/Components/UserList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function UserList() {
+  return (
+    <main className = "UserLi">
+        <div className="chatProfile"></div>
+
+    </main>
+  )
+}
+
+export default UserList

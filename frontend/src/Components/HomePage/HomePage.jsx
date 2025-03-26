8358adf99af65e686f0640c01afe2fc14c92eac0
AnshulVishwa <anshulvishwa2205@gmail.com>
2025-03-26 17:19:58 +0530

    Loading Images Quickly

--- frontend/src/Components/HomePage/HomePage.jsx
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -1,12 +1,23 @@
+import { useEffect } from 'react';
 import LeftSection from '../LeftSection/leftSection'
 import './HomePage.css'
+import SelectChat from './SelectChat'
 
 function  HomePage() {
+  const pictures = [ "image1.jpg" , "default.png" , "image2.jpg" , "image3.jpg" , "image4.jpg" , "image5.jpg" , "image6.jpg" , "image7.png" , "image8.png" , "image9.png" , "image10.jpg" , "image11.png" , "image12.jpg" , "image13.jpg" , "image14.jpg" ]
 
+  useEffect(() => {
+    pictures.forEach((pic) => {
+      const img = new Image();
+      img.src = `/${pic}`;
+      img.onload = () => console.log("Image loaded")
+    });
+  }, []);
   return (
     <>
         <main className='homeMain flex'>
-          <LeftSection />
+          <LeftSection pictures={pictures} />
+          <SelectChat />
         </main>
     </>
   )
---
+++ frontend/src/Components/HomePage/SelectChat.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./HomePage.css"
+
+function SelectChat() {
+  return (
+    <div className='noSelectedChat flex'>
+        <h1 className='selectChatText'>Select any Chat to Continue... </h1>
+    </div>
+  )
+}
+
+export default SelectChat
